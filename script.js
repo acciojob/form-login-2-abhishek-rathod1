7dc6c9a6a378637201ec98de3450931859ae106f
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-17 12:54:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+const fN = document.getElementById("firstName")
+const lN = document.getElementById("lastName")
+const pN = document.getElementById("phoneNum")
+const eI = document.getElementById("emailId")
+
+function submitInfo() {
+	let first = fN.textContent
+	let last = lN.textContent
+	let phone = pN.textContent
+	let email = eI.textContent
+
+	alert(`First Name: ${first} Last Name: ${last} Phone Number: ${phone} Email ID: ${email}`);
+	
+}
